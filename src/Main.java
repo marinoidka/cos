b898a7055a66f4641070f708c062e80a5f17c520
marinoidka <marinoidka@gmail.com>
2022-02-25 16:46:04 +0400

    static void printAnswer

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,12 @@ import pro.learnup.javaqa.group2.chernykh.Utils;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void printAnswer() {
         System.out.println(Utils.droppedOut(4));
+    }
+
+    public static void main(String[] args) {
+        printAnswer();
     }
 
 }
